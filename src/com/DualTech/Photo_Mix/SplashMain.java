1f72f2d8dbd5835529118514ec39c52e546a9311
Jessica <adajess01@gmail.com>
2015-06-09 18:36:46 +0100

    Initial commit

---
+++ src/com/DualTech/Photo_Mix/SplashMain.java
@@ -0,0 +1,18 @@
+package com.DualTech.Photo_Mix;
+
+/**
+ * Created by Jesz on 09-Jun-15.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+    }
+}
