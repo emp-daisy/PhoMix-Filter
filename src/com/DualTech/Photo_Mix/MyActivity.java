d6619e45c5e188a640644ce60cd1a70f1ec69115
tundetunde <tundetunde000@gmail.com>
2015-06-10 03:53:27 +0100

    Camera Class and functionality added.

--- src/com/DualTech/Photo_Mix/MyActivity.java
+++ src/com/DualTech/Photo_Mix/MyActivity.java
@@ -1,15 +1,52 @@
 package com.DualTech.Photo_Mix;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements View.OnClickListener{
     /**
      * Called when the activity is first created.
      */
+    ImageButton btCamera;
+    ImageButton btEditor;
+    ImageButton btGrid;
+    Intent i;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initialize();
+    }
+
+    public void initialize(){ //Initialize all Buttons
+        btCamera = (ImageButton) findViewById(R.id.cam);
+        btEditor = (ImageButton) findViewById(R.id.effect);
+        btGrid = (ImageButton) findViewById(R.id.grid);
+        btCamera.setOnClickListener(this);
+        btEditor.setOnClickListener(this);
+        btGrid.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.cam:
+                i = new Intent("com.DualTech.Photo_Mix.CAMERA");
+                startActivity(i);
+                break;
+            case R.id.effect:
+                //i = Intent("com.DualTech.Photo_Mix.EDITOR");
+                //startActivity(i);
+                break;
+            case R.id.grid:
+                //i = Intent("com.DualTech.Photo_Mix.GRID");
+                //startActivity(i);
+                break;
+        }
     }
 }
