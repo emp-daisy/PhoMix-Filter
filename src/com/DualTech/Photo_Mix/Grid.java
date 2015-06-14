6f2375a26113653a9609a237c60339941432af8d
tundetunde <tundetunde000@gmail.com>
2015-06-15 00:56:35 +0100

    Grid Class completed and works with all frames

--- src/com/DualTech/Photo_Mix/Grid.java
+++ src/com/DualTech/Photo_Mix/Grid.java
@@ -2,79 +2,108 @@ package com.DualTech.Photo_Mix;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
-/**
- * Created by tunde_000 on 10/06/2015.
- */
 public class Grid extends Activity implements View.OnClickListener {
 
     Button SaveGrid;
     Button EditGrid;
+    static int currentImgID = 0;
+    LinearLayout l1;
     Intent i;
-    ArrayList<ImageButton> imgbuttons = new ArrayList<ImageButton>();
+    ArrayList<ImageButton> imgbuttons;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.snap);
+        setContentView(R.layout.grid);
         initialize();
         initiliazeImg();
-        selectPicture();
     }
 
+    //Goes to Gallery
     public void selectPicture(){
-        i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //startActivityForResult(i, cameraData);
+        i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
     }
 
+    //Typical Initializing of buttons
     public void initialize(){
         SaveGrid = (Button) findViewById(R.id.savegrid);
         EditGrid = (Button) findViewById(R.id.grideffect);
         SaveGrid.setOnClickListener(this);
         EditGrid.setOnClickListener(this);
+        l1 = (LinearLayout) findViewById(R.id.linny);
+        imgbuttons = initiliazeImg();
+        for(ImageButton x: imgbuttons){
+            x.setOnClickListener(this);
+        }
     }
 
-    public void initiliazeImg(){
-        String[] number_as_word = {"one", "two", "three", "four", "five", "six", "seven", "eight"};
-
-
-        for(int i = 0; i < 10; i++){
-            String v = "R.id."+ number_as_word[i];
-            imgbuttons.add((ImageButton) findViewById();
+    //Initializes buttons according to how many there are
+    //Very Flexible!!
+    private ArrayList<ImageButton> initiliazeImg(){
+        ArrayList<ImageButton> imgViews = new ArrayList<ImageButton>();
+        int imgCount = l1.getChildCount();
+        for(int i = 0; i < imgCount; i++){
+            ImageButton child = (ImageButton)l1.getChildAt(i);
+            final Object tagObj = child.getTag();
+            if (tagObj != null && tagObj.equals("imgButtons")) {
+                imgViews.add(child);
+            }
         }
+        return imgViews;
     }
 
     @Override
     public void onClick(View v) {
-
-        for(int i = 0; i < 10; i++){
-            if()
-        }
         switch(v.getId()){
-            case R.id.grideffect:
+            case R.id.grideffect: //Goes to Editor
                 i = new Intent("com.DualTech.Photo_Mix.EDITOR");
                 startActivity(i);
                 break;
-            case R.id.savegrid:
+            case R.id.savegrid: //Saves the Image
 
                 break;
+        }
+        if(v.getTag().equals("imgButtons")){
+            for(ImageButton ib: imgbuttons){
+                if(v.getId() == ib.getId()) {
+                    //get currentID of image button clicked
+                    currentImgID = ib.getId();
+                }
+            }
+            selectPicture();
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //Code to use selected image
         if(resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            // my ImageView
+            ImageButton myPhotoImage = (ImageButton) findViewById(currentImgID);
+            myPhotoImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
     }
 
